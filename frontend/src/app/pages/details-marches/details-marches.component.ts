f732f75f59b5eda4a42c1b661ae4558c890cb065
Zia Belem <zbelem001@student.2ie-edu.org>
2026-04-19 04:13:17 +0000

    Amélioration onglets Details Marchés : sélection active en bleu

--- frontend/src/app/pages/details-marches/details-marches.component.ts
+++ frontend/src/app/pages/details-marches/details-marches.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { MenuComponent } from '../../components/menu/menu.component';
 
 @Component({
   standalone: true,
   selector: 'app-details-marches',
-  imports: [HeaderComponent, MenuComponent],
+  imports: [CommonModule, HeaderComponent, MenuComponent],
   templateUrl: './details-marches.component.html',
   styleUrls: ['./details-marches.component.css'],
 })
-export class DetailsMarchesComponent {}
+export class DetailsMarchesComponent {
+  activeTab = 'Informations générales';
+
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
+}
