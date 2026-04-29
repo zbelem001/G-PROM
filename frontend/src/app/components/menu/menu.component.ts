e13f89e53690b937e89be67485a478d56b11f854
Zia Belem <zbelem001@student.2ie-edu.org>
2026-04-29 03:59:23 +0000

    UI: clickable fournisseur row + active menu highlight on detail pages

--- frontend/src/app/components/menu/menu.component.ts
+++ frontend/src/app/components/menu/menu.component.ts
@@ -1,19 +1,35 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink, RouterLinkActive, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   standalone: true,
   selector: 'app-menu',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent {
-  constructor(private router: Router) {}
+  currentUrl = '';
+
+  constructor(private router: Router) {
+    this.currentUrl = this.router.url;
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event) => {
+      this.currentUrl = (event as NavigationEnd).urlAfterRedirects;
+    });
+  }
 
   navigate(path: string, event: Event) {
     console.debug('[Menu] click', path, event.type);
     event.preventDefault();
     this.router.navigateByUrl(path);
+  }
+
+  isDetailsRoute(section: 'marches' | 'fournisseurs'): boolean {
+    if (section === 'marches') {
+      return this.currentUrl.startsWith('/marches/details');
+    }
+    return this.currentUrl.startsWith('/fournisseurs/details');
   }
 }
